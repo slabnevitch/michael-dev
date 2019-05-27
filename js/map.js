11f695a068ef0bbe5a2c803e96d610ea70942611
slabnevitch <handofschpilrain@mail.ru>
2019-05-27 16:59:24 +0500

    contatcts page in progress

---
+++ js/map.js
@@ -0,0 +1,28 @@
+function initMap() {
+  michBigMap = new google.maps.Map(document.getElementById('mich-map-big'), {
+    center: {lat: 59.917632, lng: 30.526231},
+    zoom: 10,
+    disableDefaultUI: true, //отмена всех дефолтных элементов управления
+
+     // добавление необходимых элементов управления вручную
+     zoomControl: true,
+     mapTypeControl: true,
+     fullscreenControl: true
+     // styles: gmapStyles
+      // gestureHandling: 'none' //запрет на прокручивание карты
+    });
+
+  michSmMap = new google.maps.Map(document.getElementById('mich-map-sm'), {
+    center: {lat: 59.917632, lng: 30.526231},
+    zoom: 10,
+    disableDefaultUI: true, //отмена всех дефолтных элементов управления
+
+     // добавление необходимых элементов управления вручную
+     zoomControl: true,
+     mapTypeControl: true,
+     fullscreenControl: true
+     // styles: gmapStyles
+      // gestureHandling: 'none' //запрет на прокручивание карты
+    });
+
+}
